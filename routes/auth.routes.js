06f64c7f90457ef9aee520c29b0a033cc29f08a9
imason5 <ianmason5@googlemail.com>
2023-04-25 16:14:10 +0100

    Add signup

---
+++ routes/auth.routes.js
@@ -0,0 +1,50 @@
+const { Router } = require("express");
+const UserModel = require("../models/User.model");
+const router = new Router();
+
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+// GET route ==> to display the signup form to users
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup", { errorMessage: null });
+});
+// POST route ==> to process form data
+router.post("/signup", async (req, res, next) => {
+  try {
+    const potentialUser = await UserModel.findOne({
+      username: req.body.username,
+    });
+
+    if (!potentialUser) {
+      const salt = bcryptjs.genSaltSync(saltRounds);
+
+      // This variable holds the encrypted password
+      const passwordHash = bcryptjs.hashSync(req.body.password, salt);
+
+      const newUser = await UserModel.create({
+        username: req.body.username,
+        passwordHash,
+      });
+      console.log(newUser);
+
+      res.redirect("/login");
+    } else {
+      res.render("auth/signup", { errorMessage: "Username already exists" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// GET route ==> to display the login form to users
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+// POST route ==> to process form data
+
+router.post("/login", async (req, res, next) => {});
+
+module.exports = router;
